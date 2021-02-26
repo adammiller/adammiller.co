5849b3e67df295c9280f5f2acff38a256d42f521
Adam Miller <adam@heyadammiller.com>
2021-02-25 22:04:31 -0500

    Fixup unused header import

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,29 +7,15 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <style>
         @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;500&display=swap');
       </style>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-
       <main>{children}</main>
       <footer style={{
         marginTop: `2rem`
